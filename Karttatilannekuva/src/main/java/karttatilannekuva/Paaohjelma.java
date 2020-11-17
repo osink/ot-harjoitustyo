21394bd5bb2570c73a371df5a695c79e73c52f09
Ossi Inkiläinen <ossi.inkilainen@helsinki.fi>
2020-11-17 17:10:09 +0200

    fixes

--- Karttatilannekuva/src/main/java/karttatilannekuva/Paaohjelma.java
+++ Karttatilannekuva/src/main/java/karttatilannekuva/Paaohjelma.java
@@ -5,13 +5,19 @@
  */
 package karttatilannekuva;
 
+import java.util.ArrayList;
+import karttatilannekuva.domain.*;
 /**
  *
  * @author inkilaio
  */
 public class Paaohjelma {
     public static void main(String[] args){
-        System.out.println("Hello");
+        System.out.println("Syötä pisteitä komennolla 1, listaa pisteet komennolla 2");
+        ArrayList lista;
+        lista = new ArrayList<PisteObjekti>();
+        
+        
     }
     
 }
